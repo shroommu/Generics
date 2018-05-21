0e5c28a68518c737d993731d1e91e2145ac34535
Anthony Romrell <anthony@artisandream.com>
2018-05-21 14:13:50 -0600

    Adding Game State Machine.

---
+++ Scripts/GameStates/StartGame.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "StartGame")]
+public class StartGame : GameStateBase
+{
+	public override void StartState()
+	{
+		
+		Debug.Log("Starting Game");
+	}
+
+	public override void RunState()
+	{
+		Debug.Log("Running Game Start");
+	}
+}
